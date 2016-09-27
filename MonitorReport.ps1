bea211a9e2d62a298c2dbe8f246e0020a08b5fc7
raffijam <rj@triadanet.com>
2016-09-27 09:42:59 -0400

    Monitor Report

---
+++ MonitorReport.ps1
@@ -0,0 +1,13 @@
+﻿$strComputer = "."
+
+gwmi WmiMonitorID -Namespace root\wmi -computername $strComputer |
+   Select PSComputerName,
+     @{n="Model";e={[System.Text.Encoding]::ASCII.GetString($_.UserFriendlyName -ne 00)}},
+     @{n="Serial Number";e={[System.Text.Encoding]::ASCII.GetString($_.SerialNumberID -ne 00)}} |
+     Format-List | Out-File “c:\!installs\Monitors.txt”
+
+gwmi win32_videocontroller -computername $strComputer | select caption, CurrentHorizontalResolution, CurrentVerticalResolution | fl | Out-File -Append “c:\!installs\Monitors.txt”
+gwmi win32_desktopmonitor -computername $strComputer | fl MonitorManufacturer,Name | Out-File -Append “c:\!installs\Monitors.txt”
+
+
+     
